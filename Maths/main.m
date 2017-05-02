9dd443be7404879358b35ce78c34b59639e69e45
Marc Maguire <maguire.marc@gmail.com>
2017-05-02 12:25:39 -0400

    added input mechanics and string parsing conversion

--- Maths/main.m
+++ Maths/main.m
@@ -6,12 +6,33 @@
 //  Copyright © 2017 Marc Maguire. All rights reserved.
 //
 
+//Goals
+//To create a command line game called Maths that will generate a random addition question
+//To prompt the user to input their answer
+//To parse the user's inputted answer and convert it to a primitive NSInteger
+//The app will log "Right!" for correct and "Wrong!" for incorrect answers
+//The app will present the next question immediately after the app outputs the evaluation of the user's input (for now there's no exit option)
+//To add the ability to exit the game
+//To add a scoring function to the game
+
 #import <Foundation/Foundation.h>
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+        char inputChars[255];
+        printf("Input a string: ");
+        fgets(inputChars, 255, stdin);
+        NSString *result = [NSString stringWithCString:inputChars encoding:NSUTF8StringEncoding];
+        NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString:result];
+        NSString *parsedString = [result stringByTrimmingCharactersInSet:charSet];
+        
+    
+    //stringByTrimmingCharactersInSet:<#(nonnull NSCharacterSet *)#>;
+        
+        
     }
     return 0;
 }
